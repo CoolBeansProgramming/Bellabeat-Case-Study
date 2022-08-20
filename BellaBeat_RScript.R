040c2ba700a3e6e45dcb7859be7a665bf27caa34
CoolBeansProgramming <50372589+CoolBeansProgramming@users.noreply.github.com>
2022-08-20 17:11:02 -0400

    Update BellaBeat_RScript.R

--- BellaBeat_RScript.R
+++ BellaBeat_RScript.R
@@ -1,27 +1,48 @@
-# Load library 
+# Load library and files 
 library(tidyverse)
 
-# Load CSV files 
 activity <-read_csv("dailyActivity_merged.csv")
 sleep <-read_csv("sleepDay_merged.csv")
+weight <-read_csv("weightLogInfo_merged.csv")
 
 # Check that the data has loaded correctly 
 head(activity)
 head(sleep)
 
 # Convert Id to character data type 
-# Rename ActivityDate, SleepDay to Day
+# Convert Day to date format 
+# Rename various dates to Day
+
 activity <-activity %>%
   mutate_at(vars(Id), as.character) %>%
-  rename("Day"="ActivityDate")
+  mutate_at(vars(ActivityDate), as.Date, format = "%m/%d/%y") %>%
+  rename("Day"="ActivityDate") 
+  
 
 sleep <-sleep %>%
   mutate_at(vars(Id), as.character) %>%
+  mutate_at(vars(SleepDay), as.Date, format = "%m/%d/%y") %>%
   rename("Day"="SleepDay")
 
-# Combine the data
+weight <-weight %>%
+  mutate_at(vars(Id,LogId), as.character) %>%
+  mutate_at(vars(Date),as.Date, format = "%m/%d/%y") %>%
+  rename("Day"="Date")
+
+
+# Combine activity and sleep data frames
 combined_data <-sleep %>%
-  right_join(activity, by=c("Id","Day"))
+  right_join(activity, by=c("Id","Day")) %>%
+  left_join(weight, by=c("Id", "Day"))
+
+# Find and remove duplicate rows; NAs; distinct Ids
+combined_data <-combined_data[!duplicated(combined_data), ]
+
+sum(is.na(combined_data))
+
+n_distinct(combined_data$Id)
+
+# Summary Statistics 
 summary(combined_data)
 
 
